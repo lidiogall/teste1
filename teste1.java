9799af7d1072aa6ae2ed90da342bdb57ae40715a
LIDIO JUNIOR NUNES GALL <ALU201522236@uniritter.local>
2019-05-14 21:43:02 -0300

    adicao terceira mensagem

--- teste1.java
+++ teste1.java
@@ -2,6 +2,8 @@ class Teste1 {
 
 	public static void main (String[] args) {
 		system.out.println("OLá Mundo!");
+		system.out.println("Segunda mensagem!");
+		system.out.println("Terceira mensagem!");
 	}
 
 }
